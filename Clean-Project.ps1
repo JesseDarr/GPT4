ebed4e9fd2526a0347549c0a8f22643cfb6aa652
Jesse Darr <darr.jesse@gmail.com>
2023-05-11 20:28:58 -0500

    Removed all pytests, man I thought pester was bad.  Might try again later with unittest

--- Clean-Project.ps1
+++ Clean-Project.ps1
@@ -1,4 +1,4 @@
 Remove-Item ./logs/ -Recurse -Confirm:$false -ErrorAction SilentlyContinue
 Remove-Item ./tests/__pycache__/ -Recurse -Confirm:$false -ErrorAction SilentlyContinue
-Remove-Item ./tests/.pytest_cache/ -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item ./.pytest_cache/ -Recurse -Confirm:$false -ErrorAction SilentlyContinue
 Remove-Item ./modules/__pycache__/ -Recurse -Confirm:$false -ErrorAction SilentlyContinue
--- modules/utils.py
+++ modules/utils.py
@@ -4,16 +4,16 @@ class ExitException(Exception):
     # Raised when the user types 'exit' to exit the application
     pass
 
+def is_windows():
+    # Return True if the operating system is Windows, otherwise False
+    return os.name == 'nt'
+
 def clear_screen():
     # Clear the terminal screen
     if is_windows():
         clear_screen_windows()
     else:
         clear_screen_unix()
-
-def is_windows():
-    # Return True if the operating system is Windows, otherwise False
-    return os.name == 'nt'
 
 def clear_screen_windows():
     # Clear the terminal screen on Windows
